656c48fffb8b1355c4b482fab27b9bd482be0187
Henry Ishiyama <henryishiyama@gmail.com>
2022-07-07 19:20:25 +0900

    implemented until seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroy all the Restaurants"
+Restaurant.destroy_all
+
+puts "Destroy all the Reviews"
+Review.destroy_all
+
+100.times do
+  tmp_rest = Restaurant.create(
+                                name: Faker::Restaurant.name,
+                                address: Faker::Address.street_address,
+                                phone_number: Faker::PhoneNumber.phone_number,
+                                category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+              )
+
+  # Review.create(
+  #   rating: rand(0..5),
+  #   content: Faker::Restaurant.review,
+  #   restaurant: tmp_rest
+  # )
+end
+
+puts "Done..."
