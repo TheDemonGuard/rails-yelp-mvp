25f0b43edc56553890240a60933ede4b36deb452
Henry Ishiyama <henryishiyama@gmail.com>
2022-07-07 20:28:51 +0900

    Implemented Routing / Controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,55 @@
 class ReviewsController < ApplicationController
+  before_action :set_Review, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @review = Review.all
+  end
+
+  def show
+    # @review = Review.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+
+    # @review = Review.new(review_params)
+
+    # if @review.save
+    #   redirect_to review_path(@review)
+    # else
+    #   render :new
+    # end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:id, :rating, :content)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
-  resources :reviews
+  # resources :restaurants
+  # resources :reviews
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
